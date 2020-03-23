f0d10aefcc4de51765cf5afe072a81e3b5a2e76c
elthrasher <elthrasher@gmail.com>
2020-03-23 09:00:31 -0400

    correct typo

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,5 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const HttpLambdaInvokerExample = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log(event);
-  return { body: JSON.stringify({ data: 'contains valueable serverless data' }), statusCode: 200 };
+  return { body: JSON.stringify({ data: 'contains valuable serverless data' }), statusCode: 200 };
 };
