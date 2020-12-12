a3efa08eaa9b3b4f2b239bbe516247d7ef522536
elthrasher <elthrasher@gmail.com>
2020-12-12 09:57:54 -0500

    linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,13 @@
 module.exports = {
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
-    project: "./tsconfig.json",
-    sourceType: "module"
+    project: './tsconfig.json',
+    sourceType: 'module',
   },
   rules: {
-    "@typescript-eslint/interface-name-prefix": [
-      "error",
-      { prefixWithI: "never" }
-    ],
-    "@typescript-eslint/no-floating-promises": ["error"],
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/camelcase": ["error", { properties: "never" }]
-  }
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
